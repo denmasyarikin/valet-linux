d3514ba0fce8dd257c56ccde0238466b4f56384a
Carlos Priego <carlos.enrique.priego@gmail.com>
2017-09-04 22:36:07 -0500

    Base test environment detection on files

--- cli/scripts/update.sh
+++ cli/scripts/update.sh
@@ -1,30 +1,6 @@
 #!/usr/bin/env bash
 set -e
 
-function check_dependencies() {
-    local RED='\033[1;31m'
-    local NC='\033[0m'
-    local msg=''
-
-    if [[ "$APP_ENV" != "testing" ]]; then
-        for cmd in "jq" "xsel" "certutil" "NetworkManager"; do
-            local str=''
-
-            if ! [[ -x "$(command -v $cmd)" ]]; then
-                printf -v str " - %s\n" "$cmd"
-                local msg+="$str"
-            fi
-        done
-
-        if [[ $msg != '' ]]; then
-            printf "${RED}You have missing Valet dependiencies:\n"
-            printf "$msg"
-            printf "\nPlease refer to https://cpriego.github.io/valet-linux/requirements on how to install them.${NC}\n"
-            exit 1;
-        fi
-    fi
-}
-
 # Determine if the port config key exists, if not, create it
 function fix-config() {
     local CONFIG="$HOME/.valet/config.json"
@@ -44,10 +20,8 @@ function fix-config() {
 
 if [[ "$1" = "update" ]]
 then
-    check_dependencies
     composer global update "cpriego/valet-linux"
     valet install
 fi
 
-check_dependencies
 fix-config
--- tests/Functional/run.sh
+++ tests/Functional/run.sh
@@ -12,12 +12,22 @@ fi
 # Go into repository workspace
 cd $REPOSITORY
 
-sudo mkdir -p /run/NetworkManager
-echo 'nameserver 8.8.8.8' | sudo tee -a /run/NetworkManager/resolv.conf > /dev/null
-echo 'user=root' | sudo tee -a /workspace/cli/stubs/dnsmasq.conf > /dev/null
+if [[ "${APP_ENV}" = "testing" ]]; then
+    touch in_testing
+
+    if [[ ! -d '/run/resolvconf/interfaces/' ]]
+        sudo mkdir -p '/run/resolvconf/interfaces/'
+    fi
+
+    $NPATH="/run/resolvconf/interfaces/NetworkManager"
+    $DMPATH="/workspace/cli/stubs/dnsmasq.conf"
+
+    grep -pb "nameserver" "${NPATH}" || echo 'nameserver 8.8.8.8' | sudo tee -a "${NPATH}" > /dev/null
+    grep -pb "user=root" "${DMPATH}" || echo 'user=root' | sudo tee -a "${DMPATH}" > /dev/null
+fi
 
 # Install valet
 ./valet install
 
 # Run Functional tests
-./vendor/bin/phpunit --group functional
+vendor/bin/phpunit --group functional
